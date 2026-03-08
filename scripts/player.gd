53c88a4c910265215ffc285239fd7198e6bd84b0
Midnight <rickakell1@gmail.com>
2026-03-08 18:40:09 -0500

    Greatly increased game size, adjusted player movement speed and velocity

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 1000.0
+const SPEED = 1500.0
 const JUMP_VELOCITY = -400.0
+# Adjust gravity to make the game feel better
+const GRAVITY_MODIFIER = 1.3
+# Set a terminal velocity that gives you time to dodge
+const TERMINAL_VELOCITY = 2000;
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if not is_on_floor() and velocity.y <= TERMINAL_VELOCITY:
+		velocity += get_gravity() * GRAVITY_MODIFIER * delta;
+		print(velocity);
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
