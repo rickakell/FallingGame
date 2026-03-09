073336d7b289083d0a67d894f980e3d6d77b4e9b
Midnight <rickakell1@gmail.com>
2026-03-08 23:08:36 -0500

    Make the game more interesting and misc clean up

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,7 +5,7 @@ const SPEED = 1500.0
 # Adjust gravity to make the game feel better
 const GRAVITY_MODIFIER = 1.3
 # Set a terminal velocity that gives you time to dodge
-const TERMINAL_VELOCITY = 2000
+const TERMINAL_VELOCITY = 2100
 
 static var collisions = 0;
 
@@ -14,7 +14,6 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor() and velocity.y <= TERMINAL_VELOCITY:
 		velocity += get_gravity() * GRAVITY_MODIFIER * delta;
-		print(velocity);
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
