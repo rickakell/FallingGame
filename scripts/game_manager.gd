1d983c014798975de7ef693a17293516e32c4a4d
Midnight <rickakell1@gmail.com>
2026-03-08 21:03:51 -0500

    Add collision counter, game time, and speed

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,13 +2,26 @@ extends Node
 
 static var max_distance = 0;
 static var current_distance = 0;
+static var speed = 0;
+static var collisions = 0;
+static var game_time = 0;
+static var game_start_time;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	game_start_time = Time.get_unix_time_from_system();
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	current_distance = floor(abs(%Player.get_position().y) / 100);
 	max_distance = max(current_distance, max_distance);
-	$HUD/MaxDistance.text = "Max Distance: " + str(int(max_distance));
+	$HUD/MaxDistance.text = "Distance: " + str(int(max_distance));
+
+	speed = floor(%Player.velocity.y);
+	$HUD/Speed.text = "Speed: " + str(int(speed / 10));
+
+	$HUD/Collisions.text = "Collisions: " + str(%Player.collisions)
+
+	game_time = Time.get_unix_time_from_system() - game_start_time;
+	$HUD/Time.text = "Time: " + str(int(game_time));
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,22 +2,19 @@ extends CharacterBody2D
 
 
 const SPEED = 1500.0
-const JUMP_VELOCITY = -400.0
 # Adjust gravity to make the game feel better
 const GRAVITY_MODIFIER = 1.3
 # Set a terminal velocity that gives you time to dodge
-const TERMINAL_VELOCITY = 2000;
+const TERMINAL_VELOCITY = 2000
 
+static var collisions = 0;
 
+var can_collide = true
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor() and velocity.y <= TERMINAL_VELOCITY:
 		velocity += get_gravity() * GRAVITY_MODIFIER * delta;
 		print(velocity);
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -28,3 +25,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	if can_collide:
+		if get_slide_collision_count():
+			can_collide = false;
+			get_tree().create_timer(1).timeout.connect(func(): can_collide = true)
+			collisions += 1
